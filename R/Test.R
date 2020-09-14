2ff5c87bc06eba950124746b97a871ee626910fc
leokjson <70627097+leokjson@users.noreply.github.com>
2020-09-14 18:15:59 +0900

    update test code
    update test code

--- R/Test.R
+++ R/Test.R
@@ -1,8 +1,11 @@
 library(roxygen2)
 library(devtools)
 #document()
-
+print(getwd())
 #??hello
+
+df_dic <- load(file="./inst/NIADic2.Rdata")
+head(df_exam, n=10)
 install_github('leokjson/TestDicPackage', force=TRUE)
 library(TestDicPackage)
 
